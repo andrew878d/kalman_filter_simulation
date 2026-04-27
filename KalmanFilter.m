7da5ca42defada17142c872584165ca43344d81f
Andrew Dale <75088114+andrew878d@users.noreply.github.com>
2026-04-27 14:11:25 -0700

    Update to v1.4

--- KalmanFilter.m
+++ KalmanFilter.m
@@ -1,4 +1,4 @@
-%kalman filter v1.3
+%kalman filter v1.4
 %BEE499 Ghirmai Spring 2026
 
 %x is the true state. The actual position or value of the object that we
@@ -14,7 +14,16 @@ N=500; %amount of cycles to run in the simulation
 a=0.8; 
 c=0.6;
 w_variance=0.1; %process noise variance
-v_variance=0.2; %measurement noise variance
+
+%For v_variance is calculated later in terms of Psignal
+%v_variance=0.1; %measurement noise variance. 
+
+%snr array for values 0,2,4....20.
+SNR_vector = 0:2:20;
+mse_array = zeros(1, length(SNR_vector));
+
+%Note: signal to noise ratio (dB) = 10 * log (power of signal / power of
+%noise
 
 %create arrays of all 0s to store x and y values over time
 x = zeros(1, N);
@@ -24,6 +33,8 @@ y = zeros(1, N);
 %randn generates a random number with a mean of 0 and variance of 1
 x(1) = randn;
 
+
+
 %simulation loop
 for n = 2:N
 
@@ -31,63 +42,78 @@ for n = 2:N
    xnoise = sqrt(w_variance) * randn;
    %calculate next state based off the noise we just got
    x(n) = a * x(n-1) + xnoise;
+  
+end
 
-   %simulation generates a random measurement noise:
-   %this is like a real sensor measurment, but we randomize it here
-   ynoise = sqrt(v_variance) * randn;
-   y(n) = c * x(n) + ynoise;
+%for every SNR value, we need to get the mse value:
+for i = 1:length(SNR_vector)
+   
 
+    %Power = Voltage^2 / R. In this project we assume R=1. 
+    Psignal = mean((c*x).^2);
+
+    v_variance = Psignal*10^(-SNR_vector(i)/10);
+  
+    %simulation generates a random measurement noise:
+    %this is like a real sensor measurment, but we randomize it here
+    ynoise = sqrt(v_variance) * randn(1,N);
+    y = c * x + ynoise;
+
+
+    %Calculate x estimate step:
+    
+    %create an empty array of zeros for xest
+    xest = zeros(1,N);
+    
+    %first xest value is randomized
+    xest(1) = randn;
+
+    k = zeros(1, N);
+    p = zeros(1, N);
+    
+    %prediction ? how does this initial value affect the model ?
+    p(1) = 10*randn;
+
+    %this is the filter loop: estimates the truth using only sensor
+    %will be nested inside the SNR loop
+    for n=2:N
+    
+       %prediction step (time update)
+       %xest is predicted using only a
+       xest(n) = a * xest(n-1);
+       %p(n) given by formula
+       p(n) = a^2 * p(n-1) + w_variance;
+    
+       %calculate kalman gain
+       k(n) = (c * p(n)) / ((c^2 * p(n)) + v_variance);
+    
+       %measurement update step (correction step)
+       %correct the prediction
+       xest(n) = xest(n) + k(n)* (y(n) - c * xest(n));
+    
+       %then calculate from formula
+       p(n)=(1 - c * k(n)) * p(n);
+     
+    end
+
+    %after x and xest loops are ran,
+    %need to get the mean squared estimate of the error
+    %between x and xest  to tell how well it did
+    
+    %x is the truth, xest is the estimate
+    mse_filter = mean((x - xest) .^ 2);
+    
+    %also save this unique mse to snr in the array
+    mse_array(i) = mse_filter;
+   
 end
 
 
-%Calculate x estimate step:
-
-%create an empty array of zeros for xest
-xest = zeros(1,N);
-
-%first xest value is randomized
-xest(1) = randn;
-
-%prediction ? how does this initial value affect the model ?
-p(1) = 10*randn;
-
-%this is the filter loop: estimates the truth using only sensor
-for n=2:N
-
-   %prediction step (time update)
-   %xest is predicted using only a
-   xest(n) = a * xest(n-1);
-   %p(n) given by formula
-   p(n) = a^2 * p(n-1) + w_variance;
-
-   %calculate kalman gain
-   k(n) = (c * p(n)) / ((c^2 * p(n)) + v_variance);
-
-   %measurement update step (correction step)
-   %correct the prediction
-   xest(n) = xest(n) + k(n)* (y(n) - c * xest(n));
-
-   %then calculate from formula
-   p(n)=(1 - c * k(n)) * p(n);
- 
-end
-
-%after x and xest loops are ran,
-%need to get the mean squared estimate of the error
-%between x and xest  to tell how well it did
-
-%x is the truth, xest is the estimate
-mse_filter = mean((x - xest) .^ 2);
-
-%and get MSE for the sensor noise
-%from y = c*x + v, the sensor noise is v = y/c
-mse_sensor = mean((x - (y/c)) .^ 2);
-
-fprintf('MSE of filter: %f. \n', mse_filter);
-fprintf('MSE of sensor: %f. \n', mse_sensor);
+final_table = table(SNR_vector', mse_array', 'VariableNames', {'SNR (db)', 'MSE'});
+disp(final_table);
 
 figure;
-subplot(2, 1, 1);
+subplot(3, 1, 1);
 plot(1:N, x, 'r', 1:N, xest,'b')
 grid on;
 xlabel('time (n)');
@@ -95,8 +121,19 @@ ylabel('Value');
 legend('true state (x)', 'Filter estimate (xest)');
 title('standard kalman filter simulation');
 
+%plot MSE and SNR values
+subplot(3, 1, 2);
+plot(SNR_vector, mse_array, 'o-b')
+grid on;
+xlabel('SNR (db)');
+ylabel('mean squared error (MSE)');
+legend('SNR');
+title('SNR and MSE values over time');
+
+
 
 %Test experiment: increase sensor noise over time
+
 y_test = zeros(1, N);
 xest_test = zeros(1, N);
 p_test = zeros(1, N);
@@ -130,10 +167,12 @@ for n = 2:N
 
 end
 
-subplot(2, 1, 2);
+subplot(3, 1, 3);
 plot(1:N, x, 'r', 1:N, xest_test,'b')
 grid on;
 xlabel('time (n)');
 ylabel('Value');
 legend('true state (x)', 'Filter estimate (xest)');
 title('experiment: increasing sensor noise over time');
+
+
